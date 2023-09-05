6879d5ae84bf6c4ed11815559545a4e13a39806f
Brij123987 <brijeshyadav9811@gmail.com>
2023-09-05 15:53:47 +0530

    commit:10

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -6,3 +6,13 @@ class Item(models.Model):
   item_name = models.CharField(max_length=50)
   item_desc = models.CharField(max_length=300)
   items_price = models.IntegerField()
+
+  # def __str__(self):
+  #   return str((
+  #     str(self.item_name),
+  #     str(self.item_desc),
+  #     str(self.items_price)
+  #   ))
+  def __str__(self):
+    return self.item_name
+  
