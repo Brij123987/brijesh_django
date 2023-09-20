10655c46539f8de8bc7c88c71d67aa7198beca60
Brij123987 <brijeshyadav9811@gmail.com>
2023-09-20 16:21:21 +0530

    commit:37

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('details/<int:item_id>/',views.index_1, name ='index_1'),
     path('add/', views.create_item, name='create_item'),
     path('update/<int:id>/',views.update_item, name="update_item"),
+    path('delete/<int:id>/',views.delete_item, name='delete_item'),
 ]
