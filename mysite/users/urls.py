f2dba77bb3627fac36cc8a67bf600386ac2c1692
Brij123987 <brijeshyadav9811@gmail.com>
2023-10-30 16:45:44 +0530

    commit:82

--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
 
   # Orders
   path('orders/<int:id>/<int:pdcd>/<str:user>/',views.Orders, name='orders'),
+
+  #Updating Customer Order
+  path('upd_orders/<int:id>/<int:upd_order_id>/',views.update_orders, name='upd_orders'),
 ]
 
