2476803116c81cf8eef34e7cebb36da956e5fb88
Brij123987 <brijeshyadav9811@gmail.com>
2023-10-31 16:14:23 +0530

    commit:84

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -27,3 +27,21 @@ class CusOrders(models.Model):
         str(self.user)
       )
     )
+
+class CusRatingFeedback(models.Model):
+  prod_code = models.IntegerField(default=1)
+  ratings = models.FloatField()
+  feedback = models.CharField(max_length=200)
+  user_name = models.CharField(max_length=200, default='username')
+  user_type = models.CharField(max_length=200, default='Cust')
+
+  def __str__(self):
+    return str(
+      (
+        str(self.prod_code),
+        str(self.ratings),
+        str(self.feedback),
+        str(self.user_name),
+        str(self.user_type)
+      )
+    )
